cad143dc0c00c961a1c8f72d79b3701dd0f8866b
Nickolas Kenyeres <nickolas@knicklabs.com>
2024-11-15 15:36:00 -0500

    Update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,13 @@
-export default {
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
   "semi": false,
   "singleQuote": true,
   "trailingComma": "es5",
   "plugins": ["prettier-plugin-organize-imports"]
 }
+
+export default config
+
