e5d92c9327dfc5af8c1183bac9cd2f347a47efef
Nickolas Kenyeres <nickolas@knicklabs.com>
2024-11-17 16:40:50 -0500

    Setup chrome extension

---
+++ src/extension/service-worker.js
@@ -0,0 +1,17 @@
+chrome.sidePanel.setPanelBehavior({
+    openPanelOnActionClick: true
+}).catch((error) => console.error(error))
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: 'asdJobCoach',
+        title: 'ASD Job Coach',
+        contexts: ['page'],
+    })
+})
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === 'asdJobCoach') {
+        chrome.sidePanel.open({ windowId: tab.windowId })
+    }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,22 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import copy from 'rollup-plugin-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        {
+          src: 'src/extension/*',
+          dest: 'dist',
+          ignore: ['**/.js', '**/*.ts', '**/manifest.json'],
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -15,5 +27,20 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: 'src/__tests__/setup.ts'
-  }
+  },
+  build: {
+    emptyOutDir: true,
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        serviceWorker: path.resolve(__dirname, 'src/extension/service-worker.ts'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      }
+    }
+  },
 })
